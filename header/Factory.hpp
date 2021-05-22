9517445c582c408134dadd23c475221de564dff7
Jsam88 <jordansam@sbcglobal.net>
2021-05-21 17:57:24 -0700

    Modified Factory.hpp and main.cpp

--- header/Factory.hpp
+++ header/Factory.hpp
@@ -15,6 +15,7 @@
 #include <stack>
 #include <cctype>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -22,9 +23,26 @@ class Factory {
     public:
         Factory(){}
         Base* parse(char** input, int length){
-            if (length <= 1 || length % 2 != 0){
+            if (length <= 1 || length % 2 != 0){    //If the length of the arg is odd then we want to output an error msg
+                cout << "ERORR: Invalid input" << endl;
                 return nullptr;
             }
+
+            stack<Base*> OperandStack;              //make two seperate stacks, one for operands and one for operators
+            stack<string> OperatorStack;
+            double temp;
+            
+            for(int i = length -1 ; i >= 1; i--){
+                if(isdigit(*input[i])){             //Checks if the string is a digit value at i
+                temp = stod(input[i]);
+                OperandStack.push(new Op(temp));    //Push that num to the operand stack
+                }
+
+                else {
+                    OperatorStack.push(input[i]);   //else push the operator to the operator stack
+                }
+            }
+        }
 };
 
 #endif

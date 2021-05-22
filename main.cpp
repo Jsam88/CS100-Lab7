1aae1d5b37417b5e9eea94e5dd747c529476b0dc
Jsam88 <jordansam@sbcglobal.net>
2021-05-21 23:49:37 -0700

    Modified main.cpp

--- main.cpp
+++ main.cpp
@@ -26,12 +26,6 @@ using namespace std;
 int main(int argc, char** argv){
     Factory* test = new Factory();
     Base* answer = test->parse(argv, argc);
-    if (answer == nullptr){
-        cout << "Invalid Input" << endl;
-    }
-    else {
         cout << answer->stringify() << '=' << answer->evaluate() << endl;
-    }
-
     return 0;
 }
